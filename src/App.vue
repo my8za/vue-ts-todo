49dfa9a35cec3947860f318fea63909f96025eff
my8za <emilie15@naver.com>
2023-10-27 14:23:39 +0900

    투두리스트 데이터 구조화, 투두 아이템 조회 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
 <template>
-  <header>
-    <h1>Vue Todo width Typescript</h1>
-    <todo-input :item="todoText" @input="updateTodoText" @add="addTodoItem">
-    </todo-input>
-  </header>
+  <div>
+    <header>
+      <h1>Vue Todo width Typescript</h1>
+    </header>
+    <main>
+      <todo-input :item="todoText" @input="updateTodoText" @add="addTodoItem">
+      </todo-input>
+      <article>
+        <ul>
+          <todo-list-item
+            v-for="(todoItem, idx) in todoItems"
+            :key="idx"
+            :todoItem="todoItem"
+          >
+          </todo-list-item>
+        </ul>
+      </article>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+// api 역할
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: any) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    const data = JSON.parse(todoItems);
+    return data;
+  },
+};
+
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
+      todoItems: [] as any,
     };
+  },
+  created() {
+    this.fetchTodoItems();
   },
   methods: {
     updateTodoText(value: string) {
@@ -22,11 +56,15 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value);
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
       this.initTodoText();
     },
     initTodoText() {
       this.todoText = "";
+    },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
     },
   },
 });
