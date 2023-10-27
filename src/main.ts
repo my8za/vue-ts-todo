18e1d9a828f7d6dd4fc0e22923c79e7b80d982f8
my8za <emilie15@naver.com>
2023-10-27 10:57:27 +0900

    inital commit

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,12 @@ Vue.config.productionTip = false;
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
+
+/**
+ * 할 일 관리, 투두리스트 (CRUD)
+ * - 투두리스트 조회
+ * - 할 일 추가
+ * - 할 일 삭제
+ * - 할 일 수정
+ *
+ */
